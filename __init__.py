70eb39b3f3886c1fd6d7fb102667621e84746692
francois <francoisbourgeois24@gmail.com>
2023-02-01 10:34:13 +0100

    Correction importation

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-import os
+import subprocess, os
 
 try:
 	# On importe tout
-	from sftp_lib.sftp_lib import sftp_client
+	from ssh_lib.ssh_lib import ssh_client
 except ModuleNotFoundError:
 	# Si module non trouvé, on installe les dépendances
-	os.popen(f"pip install --no-cache-dir -r {os.path.dirname(os.path.realpath(__file__))}/requirements.txt").read()
-	from sftp_lib.sftp_lib import sftp_client
+	subprocess.run(f"pip install --no-cache-dir -r {os.path.dirname(os.path.realpath(__file__))}/requirements.txt", shell=True)
+	from ssh_lib.ssh_lib import ssh_client
